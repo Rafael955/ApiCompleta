1ef6f89656a6d5cbf399b71c13036f2a34d7c460
Rafael Caffonso <rafaelcaffonso@gmail.com>
2021-05-20 23:46:31 -0300

    Implementando JWT

---
+++ src/DevIO.Api/Extensions/AppSettings.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevIO.Api.Extensions
+{
+    public class AppSettings
+    {
+        /// <summary>
+        /// A chave de criptografia do JWT
+        /// </summary>
+        public string Secret { get; set; }
+
+        /// <summary>
+        /// Quantas horas o JWT irá levar até perder a validade
+        /// </summary>
+        public int ExpiracaoHoras { get; set; }
+
+        /// <summary>
+        /// Quem emite o JWT
+        /// Ex: Esta aplicação
+        /// </summary>
+        public string Emissor { get; set; }
+
+        /// <summary>
+        /// Em quais URLs este JWT é válido
+        /// </summary>
+        public string ValidoEm { get; set; }
+    }
+}
